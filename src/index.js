87dd54de6d36c38100499f25d89c05eb5fb9fe8f
shenwl <shenweilun96@gmail.com>
2017-11-25 14:21:05 +0800

    start

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(
+    <h1>Hi, React!</h1>,
+    document.getElementById('root')
+)
