9f91df6398a8e5a81435dc6577604f9b74d9ec0f
shenwl <shenweilun96@gmail.com>
2017-11-27 22:35:46 +0800

    todoList基本骨架

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      newTodo: 'test',
+      todoList: [
+        {id: 1, title: '第一个代办事项'}
+      ]
+    }
+  }
   render() {
+    let todos = this.state.todoList.map((item, index)=>{
+      return <li>{item.title}</li>
+    })
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>我的待办</h1>
+        <div className="inputWrapper">
+          <input type="text" value={this.state.newTodo} />
+        </div>
+        <ol>
+          {todos}
+        </ol>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
