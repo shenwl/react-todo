fea0d94d4b52a8423304a9e910134258cd6f3944
shenwl <shenweilun96@gmail.com>
2017-11-27 23:21:05 +0800

    update css

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-    <h1>Hi, React!</h1>,
+    <App />,
     document.getElementById('root')
 )
