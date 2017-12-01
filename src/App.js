bf02c7f735daa75105b6a9033857fbc98285a46a
shenwl <shenweilun96@gmail.com>
2017-12-01 22:07:04 +0800

    add TodoModel.create

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './css/reset.css'
 import TodoInput from './TodoInput'
 import TodoItem from './TodoItem'
 import UserDialog from './UserDialog'
-import {getCurrentUser, signOut} from './leanCloud'
+import {getCurrentUser, signOut, TodoModel} from './leanCloud'
 
 class App extends Component {
   constructor(props) {
@@ -76,15 +76,20 @@ class App extends Component {
     })
   }
   addTodo(event) {
-    this.state.todoList.push({
-      id: idMaker(),
+    let newTodo = {
       title: event.target.value,
       status: null,
-      delete: false
-    })
-    this.setState({
-      newTodo: '',
-      todoList: this.state.todoList
+      delete: false      
+    }
+    TodoModel.create(newTodo, (id) => {
+      newTodo.id = id
+      this.state.todoList.push(newTodo)
+      this.setState({
+        newTodo: '',
+        todoList: this.state.todoList
+      })
+    }, (error) => {
+      console.log(error)
     })
   }
   delete(event, todo) {
@@ -94,10 +99,3 @@ class App extends Component {
 }
 
 export default App
-
-let id = 0
-
-function idMaker() {
-  id += 1
-  return id
-}
